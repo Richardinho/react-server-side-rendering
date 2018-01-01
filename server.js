89f5f6d236bb3e763da72347218a722135326a57
Richard Hunter <richard@richardhunter.co.uk>
2018-01-01 14:55:56 +0000

    linting server code and changing README

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 let express = require('express');
-let fs = require('fs');
 let path = require('path');
 let React = require('react');
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
 let ReactDOMServer = require('react-dom/server');
 import App from './src/app';
-import renderFullPage from './render-full-page';
+import renderFullPage from './src/render-full-page';
 import reducer from './src/reducer';
 
 const app = express();
@@ -19,24 +18,23 @@ const reactApp = (store) => (
 );
 
 let handleRequest = function(req, res) {
-
-  const initialState = { phrase: 'I like Pizza' };
+  const initialState = {phrase: 'I like Pizza'};
   const store = createStore(reducer, initialState);
 
   const preloadedState = store.getState();
 
   setTimeout(() => {
-
-    const appFragment = ReactDOMServer.renderToString(reactApp(store))
+    const appFragment = ReactDOMServer.renderToString(reactApp(store));
     const html = renderFullPage(appFragment, preloadedState);
 
     res.send(html);
-
   }, 1000);
-}
+};
 
 app.use('/dist', express.static(path.join(__dirname, 'dist')));
 
 app.get('*', handleRequest);
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log('listening on port 3000');
+});
